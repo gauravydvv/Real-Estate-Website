aad1fb1644141585f24b197617f089ab22d48c40
unknown <gaurabydv0017@gmail.com>
2024-08-26 23:04:29 +0530

    img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
   return (
     <div className="flex flex-col  md:flex-row justify-between mx-6 md:mx-72 px-4 md:px-10 py-4 top-5 relative border border-[rgba(255,255,255,0.125)] rounded-xl hover:shadow-2xl hover:shadow-cyan-900 hover:transition-shadow">
       <div className="flex justify-center md:justify-start size-14 items-center mx-3 py-4 text-2xl text-white">
-        <img src="public/assets/Aroow/urllogo.jpg" alt="" />
+        <img src="assets/Aroow/urllogo.jpg" alt="" />
 
       </div>
       <div className="flex justify-center md:justify-end gap-6 mx-2 md:mx-10 mt-2 md:mt-0">
